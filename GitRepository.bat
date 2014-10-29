c98787903d9ceb9433486086ee5442a804d2f2ee
ramesh256 <botlagunta.ramesh@gmail.com>
2014-10-29 15:07:34 +0530

    Update GitRepository.bat

--- GitRepository.bat
+++ GitRepository.bat
@@ -2,6 +2,4 @@ cd c:\
 mkdir GitRepository
 cd c:\GitRepository
 git init
-git clone https://github.com/ramesh256/MergeRepo.git 
-git checkout B1
-git checkout B2
+git clone https://github.com/ramesh256/MergeRepo.git
