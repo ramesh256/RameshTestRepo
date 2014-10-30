63209c84531e1e8c84275478fa226b49351c3ce0
ramesh256 <botlagunta.ramesh@gmail.com>
2014-10-30 16:47:36 +0530

    Update GitRepository.bat

--- GitRepository.bat
+++ GitRepository.bat
@@ -4,17 +4,11 @@ cd c:\GitRepository
 git init
 git clone https://github.com/ramesh256/MergeRepo.git
 cd c:\GitRepository\MergeRepo
-git config --global push.default matching
-git config credential.helper wincred
-git push https://github.com/ramesh256/MergeRepo.git
-git Username:ramesh256
-git Password:1987oct17
+git config remote.origin.url https://ramesh256:1987oct17@github.com/ramesh256/MergeRepo.git
 git checkout B1
 git checkout B2
 git merge B1
 git push origin HEAD
-git Username:ramesh256
-git Password:1987oct1
 git checkout master
 git merge B1
 git push origin HEAD
