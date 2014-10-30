1f719f79dfaac809188572021c1e89a6bc9ec6db
ramesh256 <botlagunta.ramesh@gmail.com>
2014-10-30 15:38:42 +0530

    Update GitRepository.bat

--- GitRepository.bat
+++ GitRepository.bat
@@ -7,14 +7,14 @@ cd c:\GitRepository\MergeRepo
 git config --global push.default matching
 git config credential.helper wincred
 git push https://github.com/ramesh256/MergeRepo.git
-Username:ramesh256
-Password:1987oct17
+git Username:ramesh256
+git Password:1987oct17
 git checkout B1
 git checkout B2
 git merge B1
 git push origin HEAD
-Username:ramesh256
-Password:1987oct1
+git Username:ramesh256
+git Password:1987oct1
 git checkout master
 git merge B1
 git push origin HEAD
