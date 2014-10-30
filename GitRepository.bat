dfd55dbe3bdc2fe2af0077cfb8703ac7d77c0810
ramesh256 <botlagunta.ramesh@gmail.com>
2014-10-30 14:04:14 +0530

    Update GitRepository.bat

--- GitRepository.bat
+++ GitRepository.bat
@@ -4,6 +4,10 @@ cd c:\GitRepository
 git init
 git clone https://github.com/ramesh256/MergeRepo.git
 cd c:\GitRepository\MergeRepo
+git config credential.helper store
+git push https://github.com/ramesh256/MergeRepo.git
+Username: ramesh256
+Password: 1987oct17
 git checkout B1
 git checkout B2
 git merge B1
