22bd109ff77da3f81a486441af610e69d3598e64
ramesh256 <botlagunta.ramesh@gmail.com>
2014-10-31 14:37:28 +0530

    Update MergeFromBranch1.bat

--- MergeFromBranch1.bat
+++ MergeFromBranch1.bat
@@ -6,9 +6,10 @@ git clone https://github.com/ramesh256/MergeRepo.git
 cd c:\GitRepository\MergeRepo
 git config remote.origin.url https://ramesh256:1987oct17@github.com/ramesh256/MergeRepo.git
 git checkout Branch1
-git checkout Branch2
-git merge Branch1
-git push origin HEAD
 git checkout master
 git merge Branch1
 git push origin HEAD
+git checkout Branch2
+git merge Branch1
+git push origin HEAD
+
